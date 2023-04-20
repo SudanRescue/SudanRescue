7122cb158438b98b79ab7c6db23e4414fa87bf5d
Mahmoud Mohamed <mahmoudmoe213@gmail.com>
2023-04-20 08:03:25 -0400

    fixed index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,76 @@
-//testing github deployment
+import { useState, useEffect } from "react";
+import Head from "next/head";
+import Map from "../components/Map";
+import PostForm from "../components/PostForm";
+import PostList from "../components/PostList";
+import { supabase } from "../../supabase";
+
+export default function Home() {
+  const [posts, setPosts] = useState([]);
+  const [selectedPin, setSelectedPin] = useState(null);
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const fetchPosts = async () => {
+    const { data, error } = await supabase
+      .from("posts")
+      .select("*")
+      .order("created_at", { ascending: false });
+    if (error) console.error("Error fetching posts:", error);
+    else setPosts(data);
+  };
+
+  const handlePinDrop = (pin) => {
+    setSelectedPin(pin);
+  };
+
+  const handleSubmit = async (e, title, content) => {
+    const { data, error } = await supabase
+      .from("posts")
+      .insert([{ title, content, latitude: selectedPin.lat, longitude: selectedPin.lng }]);
+
+    if (error) {
+      console.error("Error creating post:", error);
+    } else {
+      if (data && data.length > 0) {
+        setPosts([data[0], ...posts]);
+      } else {
+        console.error("Error: No data returned after creating post");
+      }
+      setSelectedPin(null);
+    }
+  };
+
+  const handlePostClick = (post) => {
+    const location = {
+      lat: post.latitude,
+      lng: post.longitude,
+    };
+    setSelectedPin(location);
+  };
+
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <Head>
+        <title>Pin Drop App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1 className="text-3xl font-semibold">Pin Drop App</h1>
+
+      <div className="mt-8">
+        <div className="w-full h-96">
+          <Map onPinDrop={handlePinDrop} selectedLocation={selectedPin} />
+        </div>
+        {selectedPin && <PostForm onSubmit={handleSubmit} />}
+      </div>
+
+      <div className="mt-8">
+        <h2 className="text-2xl font-semibold">Recent Posts</h2>
+        <PostList posts={posts} onPostClick={handlePostClick} />
+      </div>
+    </div>
+  );
+}
